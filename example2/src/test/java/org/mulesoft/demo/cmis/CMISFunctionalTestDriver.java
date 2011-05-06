91a21ebe258538a74c8ccf467e7b8e671c9b921c
Juan Edi <juanedi@zaubersoftware.com>
2011-05-06 18:57:57 -0300

    Second demo can be triggered via HTTP

--- example2/src/test/java/org/mulesoft/demo/cmis/CMISFunctionalTestDriver.java
+++ example2/src/test/java/org/mulesoft/demo/cmis/CMISFunctionalTestDriver.java
@@ -10,16 +10,9 @@
 
 package org.mulesoft.demo.cmis;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import junit.framework.Assert;
-
-import org.apache.chemistry.opencmis.client.api.Document;
-import org.apache.chemistry.opencmis.commons.impl.dataobjects.ContentStreamImpl;
 import org.mule.api.MuleEvent;
 import org.mule.construct.SimpleFlowConstruct;
 import org.mule.tck.FunctionalTestCase;
@@ -33,28 +26,10 @@ public class CMISFunctionalTestDriver extends FunctionalTestCase
         return "mule-config.xml";
     }
 
-    public void testAlfrescoURI() throws Exception 
-    {
-        final Document doc = mock(Document.class);
-        when(doc.getId()).thenReturn("workspace://SpacesStore/96db7144-29b6-4a9e-a658-ea2b3fa1f269");
-        when(doc.getName()).thenReturn("README.txt");
-        when(doc.getContentStreamMimeType()).thenReturn("text/plain");
-        when(doc.getContentStream()).thenReturn(
-            new ContentStreamImpl("README.txt", "text/plain", "hello world"));
-        
-        final MuleEvent event = getTestEvent(doc);
-        final SimpleFlowConstruct flow = lookupFlowConstruct("documentURIFlow");
-        final MuleEvent responseEvent = flow.process(event);
-        Assert.assertEquals(
-            "http://cmis.alfresco.com/service/cmis/s/workspace:SpacesStore/i/96db7144-29b6-4a9e-a658-ea2b3fa1f269/content.txt",
-            responseEvent.getMessage().getPayload());
-    }
-
     public void testDemoFlow() throws Exception
     {
         final Map<String, String> params = new HashMap<String, String>();
         params.put("fileName", "testFile3");
-        params.put("fileUrl", "http://www.librarian.net/dnc/speeches/obama.txt");
         params.put("pastieId", "1872943");
         params.put("checkInComment", "Update text file with a speech by Obama.");
         final MuleEvent event = getTestEvent(params);
