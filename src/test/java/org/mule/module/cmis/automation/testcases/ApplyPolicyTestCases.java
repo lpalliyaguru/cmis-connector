06224eec96c46d73c3c2ac88a608b0d0274292d8
jsaliba <justin.saliba@ricston.com>
2013-08-19 12:12:26 +0200

    Added ApplyPolicy test cases

---
+++ src/test/java/org/mule/module/cmis/automation/testcases/ApplyPolicyTestCases.java
@@ -0,0 +1,69 @@
+package org.mule.module.cmis.automation.testcases;
+
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.ObjectId;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.module.cmis.VersioningState;
+
+public class ApplyPolicyTestCases extends CMISTestParent {
+
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("applyPolicy");
+
+			String rootFolderId = rootFolderId();
+			String filename = testObjects.get("filename").toString();
+			String mimeType = testObjects.get("mimeType").toString();
+			String content = testObjects.get("content").toString();
+			String objectType = testObjects.get("objectType").toString();
+			Map<String, Object> propertiesRef = (Map<String, Object>) testObjects.get("propertiesRef");
+			VersioningState versioningState = (VersioningState) testObjects.get("versioningState");
+			
+			ObjectId documentObjectId = createDocumentById(rootFolderId, filename, content, mimeType, versioningState, objectType, propertiesRef);
+			testObjects.put("documentId", documentObjectId.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testApplyPolicy() {
+		try {
+			String documentId = (String) testObjects.get("documentId");
+			testObjects.put("objectId", documentId);
+			
+			MessageProcessor flow = lookupFlowConstruct("apply-policy");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			String documentId = (String) testObjects.get("documentId");
+			delete(getObjectById(documentId), documentId, true);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}	
+}
